e3da35c1dcc6fabbdb070ad90f7a1f29a71f0005
BenGoudas <benjaming185@nycstudents.net>
2026-09-25 15:26:26 -0400

    added gambling and got an addiction

--- Game.java
+++ Game.java
@@ -4,7 +4,29 @@ public class Game {
     private Coin coin;
     public Game() {
         player = new Player(100);
-        coin = new Coin(Math.random();
-        System.out.println(Your initial balance is 100);
+        coin = new Coin(Math.random());
+        System.out.println("Your initial balance is 100");
+    }
+
+    public void play() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("How much would you like to risk?");
+        int risk = s.nextInt();
+        System.out.println("heads/tail");
+        String guess = s.next().toLowerCase();
+        boolean correct = player.flip(coin, guess, risk);
+        int balance = player.getBalance();
+        if (correct) {
+            System.out.println("Correct! Your new balance is " + balance);
+        } else {
+            System.out.println("Sorry! Your new balance is " + balance);
+        }
+        if (balance > 0) {
+            play();
+        }
+        else {
+            System.out.println("GAME OVER!");
+            s.close();
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args){
+        Game g = new Game();
+        g.play();
+        
+
+
         Coin penny = new Coin();
         Player player = new Player(100);
         player.flip(penny, "tails", 50);
